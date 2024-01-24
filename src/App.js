5c48f31448121b2c80d8886a6652a8a18b11ee59
Ahmed Khalid <ahmed.khalid@koderlabs.com>
2024-01-24 14:13:10 +0500

    Basic Router & Basic Screen Created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import './App.css';
+import Body from './components/Body';
 
 function App() {
   return (
-    <div className="App">
-      <p className='text-3xl font-bold text-red-800 text-center'>Ahmed here</p>
-    </div>
+    <Body />
   );
 }
 
