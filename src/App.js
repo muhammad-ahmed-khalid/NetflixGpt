43bd1f6284f1d469c198f79121ebbf140105e947
Ahmed Khalid <ahmed.khalid@koderlabs.com>
2024-01-24 13:54:35 +0500

    Tailwind Implemneted

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <p>Ahmed here</p>
+      <p className='text-3xl font-bold text-red-800 text-center'>Ahmed here</p>
     </div>
   );
 }
