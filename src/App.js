6b63e081023512abcc11a214e5cabdfe4b0b6aeb
Ahmed Khalid <ahmed.khalid@koderlabs.com>
2024-01-27 15:45:03 +0500

    React Redux Store Setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { Provider } from 'react-redux';
 import './App.css';
 import Body from './components/Body';
+import appStore from './utility/appStore';
 
 function App() {
   return (
+    <Provider store={appStore}>
     <Body />
+    </Provider>
   );
 }
 
