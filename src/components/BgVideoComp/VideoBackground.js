de58dc3dde48a462dfcaae420c9d4ba51b7b9f7b
Ahmed Khalid <ahmed.khalid@koderlabs.com>
2024-01-31 01:49:35 +0500

    Littie Bit Styling

--- src/components/BgVideoComp/VideoBackground.js
+++ src/components/BgVideoComp/VideoBackground.js
@@ -17,13 +17,13 @@ function VideoBackground({ movieID }) {
   }, []);
 
   return (
-    <div className="absolute w-full h-full bg-red-500 top-0 left-0 bg-opacity-15 -z-10">
+    <div className="absolute w-full h-full top-0 left-0 bg-opacity-15 -z-10">
       <iframe
         width="100%"
         height="100%"
         src="https://www.youtube.com/embed/UGc5Tzz19UY"
-        title="YouTube video player"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        title="Netflix-GPT"
+        // allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
       ></iframe>
     </div>
   );
