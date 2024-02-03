01cb3e55b1d5f1e93a3fc4ad73d69ff3ee371a4b
Ahmed Khalid <ahmed.khalid@koderlabs.com>
2024-02-03 19:11:12 +0500

    Issue Done

--- src/components/BgVideoComp/VideoInfo.js
+++ src/components/BgVideoComp/VideoInfo.js
@@ -3,7 +3,7 @@ import React from "react";
 function VideoInfo({ data }) {
   const { title, overview, adult } = data || {};
   return (
-    <div className="h-screen px-4 w-full md:h-auto relative aspect-video md:pl-12 bg-gradient-to-r from-black flex flex-col justify-center">
+    <div className="h-screen pl-4 w-full md:h-auto relative aspect-video md:pl-12 bg-gradient-to-r from-black flex flex-col justify-center">
       <h1 className="md:text-5xl text-white font-bold">{title}</h1>
       <div className="relative">
         <div className="md:w-4/12 w-full">
@@ -15,7 +15,7 @@ function VideoInfo({ data }) {
           </button>
           <p className="text-white text-xs md:text-sm ">{overview}</p>
         </div>
-        <p className="absolute bottom-0 right-0 text-white px-4 py-2 bg-slate-400 bg-opacity-25 border-l-2 border-white">
+        <p className="absolute -bottom-20 md:bottom-0 right-0 text-white px-4 py-2 bg-slate-400 bg-opacity-25 border-l-2 border-white">
           {adult ? "+ 18" : "Family"}
         </p>
       </div>
